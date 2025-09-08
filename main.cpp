7f314d990dda422a69796d8a4f334c9b738ce6d2
wanngumi <wanngumi@ioiio.cc>
2025-09-08 11:23:54 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_log.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "start" << std::endl;
+
+    SDL_Log("TT TEsdt");
+
+    return 0;
+}
